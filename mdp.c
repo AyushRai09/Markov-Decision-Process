47429bb0a4c050c64a529e60b53837b6c9f5dbad
AyushRai09 <ayushrai982@gmail.com>
2017-03-11 11:42:40 +0530

    Final version of mdp.

--- mdp.c
+++ mdp.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-float arr[5][5]={0};
-float temp_board[4][4];
+float arr[6][6]={0};
+float temp_board[6][6];
 int func( const void *a, const void *b)
 {
 	return ( *(int*)a- *(int*)b );
@@ -23,9 +23,14 @@ int checkEndCondition(float delta)
 	int i, j, flag=0;
 	for(i=1;i<=4;i++)
 		for(j=1;j<=4;j++)
+		{
+			if((i==1 && j==1) || (i==1 && j==2) || (i==1 && j==3) || (i==1 && j==4) || (i==3 && j==2) || (i==3 && j==3))
+				continue;
 			if(fabs(arr[i][j]-temp_board[i][j])>delta)
+			{
 				flag=1;
-
+			}
+		}
 	return flag;
 }
 void copyTempIntoArr()
@@ -44,28 +49,31 @@ void copyArrIntoTemp()
 }
 float maxOfAllDirections(float a, float b, float c, float d)
 {
+	int i;
 	float comp[4];
 	comp[0]=a;
 	comp[1]=b;
 	comp[2]=c;
 	comp[3]=d;
-	qsort(comp,4,sizeof(float),func);
-	return comp[0];
+	float max=-10000007;
+	for(i=0;i<4;i++)
+		if(comp[i]>max)
+			max=comp[i];
+	return max;
 }
 int main()
 {
 	int i, j, k, n;
 	int team_number;
-	float delta, cost,goingEastValue,goingWestValue,goingNorthValue,goingSouthValue;
+	float delta, cost, goingEastValue,goingWestValue,goingNorthValue,goingSouthValue;
+	printf("Enter your Team Number:");
 	scanf("%d", &team_number);
-	delta=(1/20)*team_number;
-	cost=(-1/20)*team_number;
-
+	delta=0.05*team_number;
+	cost=(-0.05)*team_number;
 	arr[1][3]=team_number;
 	arr[3][2]=-team_number;
 
-	//arr[0][0]=0;arr[0][1]=0;arr[0][2]=0;arr[0][3]=0;arr[0][4]=0;arr[0][5]=0;arr[1][0]=0;arr[2][0]=0;arr[3][0]=0;arr[4][0]=0;arr[5][0]=0;arr[5][1]=0;arr[5][2]=0;arr[5][3]=0;arr[5][4]=0;arr[5][5]=0;arr[1][5]=0;arr[2][5]=0;arr[3][5]=0;arr[4][5]=0;arr[5][5]=0;
-
+	printf("The iterations are:\n");
 	print_board();
 	do
 	{
@@ -81,10 +89,15 @@ int main()
 				goingSouthValue=cost+arr[i+1][j]*0.8 + arr[i][j-1]*0.1 + arr[i][j+1]*0.1;
 				temp_board[i][j]=maxOfAllDirections(goingEastValue,goingWestValue,goingNorthValue,goingSouthValue);
 			}
-		copyTempIntoArr();
+		if(checkEndCondition(delta))
+			copyTempIntoArr();
+		else 
+			break;
 		print_board();
 
-	}while(checkEndCondition(delta));
+	}while(1); 
+	copyTempIntoArr();
+	print_board();
 	return 0;
 }
 
